addcfd1ca5aa6420558ec450009d61970e5220f0
Jasper Thayer <info@jasperthayer.com>
2018-03-07 15:29:38 -0800

    Small style chage to User component

--- src/User/User.js
+++ src/User/User.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './User.css';
 
 const user = (props) => {
-    return <div className="user"><b>Name:</b> {props.name} <b>Title:</b> {props.title} <button>X</button></div>
+    return <div className="user"><b>Name:</b> {props.name} <b>Title:</b> {props.title} <button><b>X</b></button></div>
 };
 
 export default user;
