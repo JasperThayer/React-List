5a99848b17494ccc8363dc3997f2f223b4e66a1c
Jasper Thayer <info@jasperthayer.com>
2018-03-07 15:35:36 -0800

    Cleaning up User style

--- src/User/User.js
+++ src/User/User.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './User.css';
 
 const user = (props) => {
-    return <div className="user"><b>Name:</b> {props.name} <b>Title:</b> {props.title} <button><b>X</b></button></div>
+    return (
+        <div className="user">
+            <b>Name:</b> {props.name}<br/>
+            <b>Title:</b> {props.title}<br/>
+            <button><b>X</b></button>
+        </div>
+    );
 };
 
 export default user;
